7a3ac881779ce9f9a8312afe707150629d8e20e3
Pupha <pupha.pomn@kmutt.ac.th>
2022-09-14 19:32:50 +0700

    test01

--- concatArray.js
+++ concatArray.js
@@ -2,5 +2,33 @@ const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
   //code here
+  // if (array1 == null && array2 == null) {
+  //   return undefined
+  // }
+  // else if (array1 == undefined && array2 == undefined) {
+  //   return undefined
+  // }
+  // else if (array1.length == 0 && array2.length == 0) {
+  //   return 'empty array'
+  // }
+  // else {
+  //   if (array1 !== ) {
+      
+  //   }
+  // }
+  if((array1 == null || array1 == undefined)&& (array2 == null ||array2 == undefined)){
+    return undefined
+  }
+  else{
+    if(array1 ==null || array1 == undefined){
+      return array2
+    }
+    else if(array2==null || array2 == undefined){
+      return array1
+    }
+    else{
+      return array1.concat(array2)
+    }
+  }
 }
 module.exports = concatArray
